8d2d8d50c2be7d08f7a97ef890ef675943b010b8
ritvikmehra17 <46188917+ritvikmehra17@users.noreply.github.com>
2024-03-17 17:22:32 +0530

    Add files via upload

---
+++ carhub/force-app/main/default/lwc/placeholder/placeholder.js
@@ -0,0 +1,9 @@
+
+import { LightningElement, api } from 'lwc';
+/*static resource*/
+import CAR_HUB_PLACEHOLDER from '@salesforce/resourceUrl/placeholder'
+export default class Placeholder extends LightningElement {
+    @api message
+
+    placeholderUrl = CAR_HUB_PLACEHOLDER
+}
